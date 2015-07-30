0a69542859b1936be54b12707aaf2df284f73ed1
Yannick Calleiro <yannick@calleiro.com>
2015-07-30 17:32:16 -0400

    Added method to deal with empty youtube url field

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,6 +7,10 @@ class Artist < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   def video_info
-	return VideoInfo.new(youtube_address)
+  	if youtube_address.present?
+	  return VideoInfo.new(youtube_address)
+  	else
+  	  return nil
+	end
   end
 end
