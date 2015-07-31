825d63191e2d11d6e819f35f16d7911ee3b5b646
Yannick Calleiro <yannick@calleiro.com>
2015-07-31 11:51:02 -0400

    added AWS gem to save images in heroku

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+  }
+}
